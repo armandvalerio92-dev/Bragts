d91821ee6fafbe919a9589f3e7391e5d72dc9a05
armandvalerio92-dev <armandvalerio92@gmail.com>
2026-07-13 11:06:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
   'https://tracxs.netlify.app',
   'https://bellbusinesscanada.netlify.app',
   'https://sunny-pastelito-8843b7.netlify.app',
-  'https://businessserve.netlify.app',
+  'https://bizbill.netlify.app',
   'https://jhvnb.vercel.app',
    // add all your trusted frontends here
 ];
