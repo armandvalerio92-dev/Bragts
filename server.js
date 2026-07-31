b9b200521d1eb7edaad1aee632ebff78af26dc9f
armandvalerio92-dev <armandvalerio92@gmail.com>
2026-07-31 18:41:02 -0400

    Replace brunkk.netlify.app with sittin.netlify.app

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
   'https://swiftyl.netlify.app',
   'https://bellbusinesscanada.netlify.app',
   'https://sunny-pastelito-8843b7.netlify.app',
-  'https://brunkk.netlify.app',
+  'https://sittin.netlify.app',
   'https://jhvnb.vercel.app',
    // add all your trusted frontends here
 ];
